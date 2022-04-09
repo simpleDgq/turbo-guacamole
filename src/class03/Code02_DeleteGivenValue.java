3739f72eaea0c3f8c9ea0f82ee399a6d7534c1eb
gqdeng <gqdeng@cn.ibm.com>
2022-04-09 21:19:06 +0800

    Delete given value in list

---
+++ src/class03/Code02_DeleteGivenValue.java
@@ -0,0 +1,53 @@
+package class03;
+
+public class Code02_DeleteGivenValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public class Node {
+		int value;
+		Node next;
+		public Node(int value) {
+			this.value = value;
+			this.next = null;
+		}
+	}
+	
+	// 删除链表中给定的元素
+	/**
+	 * 思路: 1. 从头结点开始找到第一个不需要删除的位置
+	 * 		2. 定义两个只在pre和cur,判断cur指向的位置是否需要删除，如果需要，则
+	 * 			pre.nex = cur.next; 
+	 * 			cur = cur.next;
+	 * 		  如果不需要删除，
+	 * 			pre = cur;
+	 * 			cur = cur.next;
+	 * 
+	 * @param head
+	 * @param value
+	 * @return
+	 */
+	public static Node deleteGivenValue(Node head, int value) {
+		// 找到第一个不需要删除的位置，head指针移动
+		while(head != null) {
+			if(head.value != value) {
+				break;
+			}
+			head = head.next;
+		}
+		Node pre = head;
+		Node cur = head;
+		while(cur != null) {
+			if(cur.value == value) {
+				pre.next = cur.next;
+			} else {
+				pre = cur;
+			}
+			cur = cur.next;
+		}
+		return head;
+	}
+}

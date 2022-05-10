bdc2db2289abb1a317031369b5d160497ad73a03
gqdeng <gqdeng@cn.ibm.com>
2022-05-10 17:18:07 +0800

    注释

--- src/class17/Code04_ReverseStackByRecursive.java
+++ src/class17/Code04_ReverseStackByRecursive.java
@@ -7,7 +7,7 @@ public class Code04_ReverseStackByRecursive {
 	 * 给定一个栈，请逆序这个栈，不能申请额外的数据结构，只能使用递归函数
 	*	（微软）
 	*  1. 有一个函数f，能够获取栈最底部的元素，并且将上面的元素盖下来
-	*  2. 调用f
+	*  2. 调用f之后，递归reverseStack，push f的返回值到stack
 	 */
 	
 	public static int f(Stack<Integer> stack) {

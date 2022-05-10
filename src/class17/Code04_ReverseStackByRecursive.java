a232220433f0e1ed2dfc1c65a782d26d2114e53e
gqdeng <gqdeng@cn.ibm.com>
2022-05-10 17:14:07 +0800

    使用递归，将栈逆序

---
+++ src/class17/Code04_ReverseStackByRecursive.java
@@ -0,0 +1,32 @@
+package class17;
+
+import java.util.Stack;
+
+public class Code04_ReverseStackByRecursive {
+	/**
+	 * 给定一个栈，请逆序这个栈，不能申请额外的数据结构，只能使用递归函数
+	*	（微软）
+	*  1. 有一个函数f，能够获取栈最底部的元素，并且将上面的元素盖下来
+	*  2. 调用f
+	 */
+	
+	public static int f(Stack<Integer> stack) {
+		int result = stack.pop();
+		if(stack.isEmpty()) {
+			return result;
+		} else {
+			int last = f(stack);
+			stack.push(result);
+			return last;
+		}
+	}
+	
+	public static void reverseStack(Stack<Integer> stack) {
+		if(stack.isEmpty()) {
+			return;
+		}
+		int i = f(stack);
+		reverseStack(stack);
+		stack.push(i);
+	}
+}

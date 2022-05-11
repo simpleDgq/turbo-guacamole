0c040f951b513bce5ebe433fce31c7d453b7ff9b
gqdeng <gqdeng@cn.ibm.com>
2022-05-11 10:17:21 +0800

    qucikSort

---
+++ src/class05/Code01_QuickSort.java
@@ -0,0 +1,30 @@
+package class05;
+
+public class Code01_QuickSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * 快排1.0
+	 * 
+	 * <= X的放左边，> X的放右边
+	 * 
+	 * 思路：用数组的最后一个元素作为划分值
+	 * 有一个小于区，从-1位置开始
+	 */
+	
+	/**
+	 * 快排2.0 ==> 荷兰国旗问题
+	 * 
+	 *  < X的放左边，=X的放中间，> X的放右边
+	 */
+	
+	
+	/**
+	 * 快排3.0 ==> 随机快排
+	 */
+
+}

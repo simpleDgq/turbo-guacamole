0bde8210fc03efa410ff9e742a53d48e99008694
gqdeng <gqdeng@cn.ibm.com>
2023-04-14 16:01:09 +0800

    Update comments

--- src/class07/Code03_HeapGreater.java
+++ src/class07/Code03_HeapGreater.java
@@ -6,16 +6,32 @@ import java.util.HashMap;
 
 public class Code03_HeapGreater {
 	/**
+	 * 一个堆中的对象，如果某个属性发生了变化，堆又是基于这个属性来排序的，
+	 * 系统提供的堆没有提供一种功能，当这个对象发生变化的时候，自动调整堆，
+	 * 将这个对象向上或者向下调整。因为系统提供的堆，不知道改变的元素的位置。
+	 * 
+	 * 设计一张反向索引表，来记录每个元素的位置。
+	 * 当某个元素发生了变化之后，通过反向索引表查到发生变化的位置，
+	 * 然后去向上或者向下调整该位置的元素。
+	 * 
+	 * 添加或修改元素之后，或在swap元素的过程中，修改反向索引表，
+	 * 记录该元素的位置
+	 * 
 	 * 手动改写堆
 	 * 1. 任意修改一个位置的元素，能够自动调整堆
 	 * 2. 能够任意删除一个位置的元素，删除之后也能够自动调整堆 
+	 * 
+	 * 4个重要的方法需要实现:
+	 * push: 放入堆中的同时，反向索引表也记录下位置
+	 * pop: 要从反向索引表中删除对应的元素索引
+	 * remove: remove的时候，将要被remove的元素和最后一个元素交换，然后调用resign重新调整堆
+	 * resign: 某个元素obj发生了改变，重新调整堆. 调用heapInsert和heapIfy，只会调用一个
 	 */
-	
 	public static class HeapGreater<T> {
 		private ArrayList<T> heap; // 堆，存放元素
 		private int heapSize;
-		private HashMap<T, Integer> indexMap; // 反向索引表， 记录每个元素在对中的位置
-		private Comparator<T> comparator; // 比较器，定义加入堆中的元素的比较规则
+		private HashMap<T, Integer> indexMap; // 反向索引表， 记录每个元素在堆中的位置
+		private Comparator<T> comparator; // 比较器，定义加入堆中的元素的比较规则。 在heapInsert和heapIfy中，用比较器比较节点
 		
 		public HeapGreater(Comparator<T> comparator) {
 			this.heap = new ArrayList<T>();
@@ -27,7 +43,6 @@ public class Code03_HeapGreater {
 		public boolean isEmpty() {
 			return this.heapSize == 0;
 		}
-		
 		
 		public int size() {
 			return this.heapSize;
@@ -56,7 +71,7 @@ public class Code03_HeapGreater {
 			heap.remove(heapSize - 1); // 从堆中删除最后一个元素
 			indexMap.remove(ans); // 从反向索引表中删除对应的元素索引
 			heapSize--;
-			heapIfy(0); // 向下调整对
+			heapIfy(0); // 向下调整堆
 			return ans;
 		}
 		
@@ -74,6 +89,7 @@ public class Code03_HeapGreater {
 			// 重新调整堆
 			resign(obj);
 		}
+		
 		/**
 		 * 某个元素obj发生了改变，重新调整堆
 		 * 向上或者向下调整
@@ -85,9 +101,9 @@ public class Code03_HeapGreater {
 			heapIfy(index);
 		}
 		
-		
 		public void heapInsert(int index) {
-			while(comparator.compare(heap.get(index), heap.get((index - 1) / 2)) < 0) { // 如果新加入的节点比它的父节点小，则交换，继续往上
+			// 如果新加入的节点比它的父节点小，则交换，继续往上
+			while(comparator.compare(heap.get(index), heap.get((index - 1) / 2)) < 0) {
 				swap(index, (index - 1) / 2);
 				index = (index - 1) / 2;
 			}
@@ -100,8 +116,8 @@ public class Code03_HeapGreater {
 				int largestIndex = left + 1 < heapSize ? // 存在右子节点
 													comparator.compare(heap.get(left), heap.get(left + 1)) < 0 ? left + 1 : left // 右子节点比左子节点大 或者小 
 													: left; // 不存在右子节点
-				
-				if(comparator.compare(heap.get(largestIndex), heap.get(index)) < 0) { // 如果子节点较大的，还小于等于当前节点，那么直接退出，不用交换
+				// 如果子节点较大的，还小于等于当前节点，那么直接退出，不用交换
+				if(comparator.compare(heap.get(largestIndex), heap.get(index)) < 0) {
 					break;
 				}
 				swap(index, largestIndex); // 与较大的子节点进行交换
@@ -109,12 +125,15 @@ public class Code03_HeapGreater {
 			} 
 		}
 		
+		/**
+		 * 交换过程中注意要更新索引表
+		 */
 		public void swap(int i, int j) {
 			T temp = heap.get(i);
 			heap.set(i, heap.get(j));
 			heap.set(j, temp);
 			// 更新反向索引表中，元素的位置
-			indexMap.put(heap.get(j), j);
+			indexMap.put(heap.get(j), j); // 堆交换完之后，j位置的数就是heap.get(j)
 			indexMap.put(heap.get(i), i);
 		}
 	}

974972174933e842e63db9e85e00089c06be8c5a
gqdeng <gqdeng@cn.ibm.com>
2023-04-16 17:12:37 +0800

    Update comments

--- src/class09/Code01_LinkedListMid.java
+++ src/class09/Code01_LinkedListMid.java
@@ -3,11 +3,16 @@ package class09;
 public class Code01_LinkedListMid {
 	
 	/**
-	题目：
-	1. 如果链表是奇数个节点，则返回唯一的中点；如果是偶数个节点，则返回上中点
-	2. 如果链表是奇数个节点，则返回唯一的中点；如果是偶数个节点，则返回下中点
-	3. 如果是奇数个，返回中点的前一个节点；如果是偶数个节点，则返回上中点的前一个节点
-	4. 如果是奇数个，返回中点的前一个节点；如果是偶数个节点，则返回上中点
+	 * 题目：
+	 * 1. 如果链表是奇数个节点，则返回唯一的中点；如果是偶数个节点，则返回上中点
+	 * 2. 如果链表是奇数个节点，则返回唯一的中点；如果是偶数个节点，则返回下中点
+	 * 3. 如果是奇数个，返回中点的前一个节点；如果是偶数个节点，则返回上中点的前一个节点
+	 * 4. 如果是奇数个，返回中点的前一个节点；如果是偶数个节点，则返回上中点
+	 * 
+	 * 快慢指针解决，
+	 * 一个慢指针，每次走一步
+	 * 一个快指针，每次走两步
+	 * 快指针走完的时候，慢指针，就正好落在中点的位置
 	*/
 	public static class Node {
 		int value;
@@ -28,8 +33,8 @@ public class Code01_LinkedListMid {
 		Node slow = head;
 		Node fast = head;
 		while(fast != null && fast.next != null) {
-			fast = fast.next.next;
-			if(fast != null) {
+			fast = fast.next.next; // fast一次走两步
+			if(fast != null) { // fast没有到达终点，slow向下走一步
 				slow = slow.next;
 			}
 		}
@@ -37,7 +42,7 @@ public class Code01_LinkedListMid {
 	}
 	
 	/**
-	 * 	2. 如果是奇数个，返回中点的前一个节点；如果是偶数个节点，则返回上中点的前一个节点
+	 * 	2. 如果链表是奇数个节点，则返回唯一的中点；如果是偶数个节点，则返回下中点
 	 */
 	public static Node midOrDownMid(Node head) {
 		if(head == null || head.next == null) {
@@ -53,9 +58,10 @@ public class Code01_LinkedListMid {
 	}
 	
 	/**
-	 * 	3. 如果是奇数个，不返回中点，返回中点的前一个节点；如果是偶数个节点，则返回上中点的前一个点
+	 * 	3. 如果是奇数个，返回中点的前一个节点；如果是偶数个节点，则返回上中点的前一个节点
 	 */
 	public static Node upMidOrUpMidPre(Node head) {
+		// 要求中点的前一个节点，就必须至少有两个节点
 		if(head == null || head.next == null || head.next.next == null) {
 			return null;
 		}

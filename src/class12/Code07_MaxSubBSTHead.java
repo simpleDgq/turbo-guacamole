9694e94025739c8ab8ecc46f10486cf250ac2daf
gqdeng <gqdeng@cn.ibm.com>
2023-04-19 21:52:10 +0800

    Update comments

--- src/class12/Code07_MaxSubBSTHead.java
+++ src/class12/Code07_MaxSubBSTHead.java
@@ -73,6 +73,7 @@ public class Code07_MaxSubBSTHead {
 		Info leftInfo = process(X.left);
 		Info rightInfo = process(X.right);
 		// X为头的整棵树，构造info
+		// 以X为头的整棵树，求最大和最小值
 		Node head = null;
 		int max = X.value;
 		int min = X.value; // 以X为头的整棵树的最小值
@@ -88,25 +89,33 @@ public class Code07_MaxSubBSTHead {
 			min = Math.min(min, rightInfo.min);
 			size += rightInfo.size;
 		}
+		// 左树最大搜索二叉树
 		int p1 = -1;
 		Node headLeft = null;
 		if(leftInfo != null) {
 			p1 = leftInfo.maxSubSize;
 			headLeft = leftInfo.head; // 左树最大搜索二叉树头
 		}
+		// 右树最大搜索二叉树
 		int p2 = -1;
 		Node headRight = null;
 		if(rightInfo != null) {
 			p2 = rightInfo.maxSubSize;
 			headRight = rightInfo.head; // 右树最大搜索二叉树头
 		}
+		// 以X为头的整棵树搜索二叉树
 		int p3 = -1;
 		Node headX = null;
+		// X为头的左右子树是否是搜索二叉树
 		boolean isLeftBST = leftInfo == null ? true : leftInfo.maxSubSize == leftInfo.size;
 		boolean isRightBST = rightInfo == null ? true : rightInfo.maxSubSize == rightInfo.size;
+		// 如果左右子树都是搜索二叉树
 		if(isLeftBST && isRightBST) {
+			// 左子树的最大值是否小于X节点的值
 			boolean leftMaxLessX = leftInfo == null ? true : leftInfo.max < X.value;
+			// 右子树的最小值是否大于X节点的值
 			boolean rightMinMoreX = rightInfo == null ? true : rightInfo.min > X.value;
+			// 如果是，说明以X为头的整棵树都是搜索二叉树，求整颗树的节点数量
 			if(leftMaxLessX && rightMinMoreX) {
 				int leftSize = leftInfo == null ? 0 : leftInfo.size;
 				int rightSize = rightInfo == null ? 0 : rightInfo.size;
@@ -114,7 +123,10 @@ public class Code07_MaxSubBSTHead {
 				headX = X;
 			}
 		}
+		// 左子树的最大搜索二叉树、右子树的最大搜索二叉树、以X为头结点的搜索二叉树
+		// 三者PK出最大值
 		maxSubSize = Math.max(p1, Math.max(p2, p3));
+		// 看最大值等于哪个size,返回对应的搜索二叉树的头节点
 		if(maxSubSize == p1) {
 			head = headLeft;
 		} else if(maxSubSize == p2) {

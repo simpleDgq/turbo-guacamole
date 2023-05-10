7fa1cdca30215c9a285d19d83a5533e4335dc3df
gqdeng <gqdeng@cn.ibm.com>
2023-05-10 15:43:30 +0800

    update comments

--- src/class19/Code02_ConvertNumToStr.java
+++ src/class19/Code02_ConvertNumToStr.java
@@ -9,15 +9,15 @@ public class Code02_ConvertNumToStr {
 	 * 给定一个只有数字字符组成的字符串str，返回有多少种转化结果
 	 */
 	// 1. 憋尝试
-	// 从左往右尝试模型。每一个数字都可以要或者不要，每一中情况都枚举，就是答案
-	// 当前来到index位置，index之前的已经确定了，不用管，index及其之后的位置的字符都可以单转或者和下一个字符合在一起转
-	// ，给我返回，有多少中转化结果
 	public static int number(String str) {
 		if (str == null || str.length() == 0) {
 			return 0;
 		}
 		return process(str.toCharArray(), 0);
 	}
+	// 从左往右尝试模型。每一个数字都可以要或者不要，每一种情况都枚举，就是答案
+	// 当前来到index位置，index之前的已经确定了，不用管，index及其之后的位置的字符都可以单转或者和下一个字符合在一起转
+	// ，给我返回，有多少种转化结果
 	public static int process(char arr[], int index) {
 		if(index == arr.length) { // 没有字符了，前面已经形成的转化就是一种方案
 			return 1;
@@ -25,9 +25,9 @@ public class Code02_ConvertNumToStr {
 		if(arr[index] == '0') { // 上一次的决策有错
 			return 0;
 		}
-		// 当前字符单转
+		// 当前字符单转，直接考虑下一个字符
 		int ways = process(arr, index + 1);
-		// 和下一个字符和在一起转化
+		// 当前字符和下一个字符和在一起转化
 		// index + 1可能越界，或者和index + 1拼接在一起后，超过了26（总共只有26个字符） ==> 这两种情况，都不应该是一种转化方案
 		if(index + 1 < arr.length &&
 				((arr[index] - '0') * 10 + (arr[index + 1] - '0')) <= 26) {
@@ -36,7 +36,7 @@ public class Code02_ConvertNumToStr {
 		return ways;
 	}
 	
-	// 2. 可变参数inde，范围: 0~N ==> int dp[N + 1]
+	// 2. 可变参数index，范围: 0~N ==> int dp[N + 1]
 	// 分析依赖，看尝试，依赖(index + 1 ==> 依赖后一个元素 ==> 从右往左初始化
 	public static int number2(String str) {
 		if (str == null || str.length() == 0) {
